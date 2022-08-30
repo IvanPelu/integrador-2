b1c81838a95603074e6fdfa486e15a977464ea94
IvanPelu <103153127+IvanPelu@users.noreply.github.com>
2022-08-30 18:37:33 -0300

    Add files via upload

---
+++ Ivan-Pelli-Trabajo-Integrador/Rivin/Html/Uusario.js
@@ -0,0 +1,9 @@
+let usuario = ""
+
+function IdentificarUsuario(){
+   let respuesta = prompt("ingrese su usuario")
+   if (usuario === Number || usuario === ""){
+   console.log("su usuario es: " + respuesta);
+}
+}
+IdentificarUsuario()
---
+++ Ivan-Pelli-Trabajo-Integrador/Rivin/Html/Visitas.js
@@ -0,0 +1,5 @@
+let visitas = 0
+
+for (let i = 0; i < 1000;i++){
+    console.log(i)
+}
